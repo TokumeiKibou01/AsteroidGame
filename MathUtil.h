f99d5648d21428a6513e0034061c43b3765d65de
TokumeiKibou01 <snowsansub070113@gmail.com>
2026-02-24 12:46:47 +0900

    ＜追加＞ MathUtilの関数を追加しました

--- MathUtil.h
+++ MathUtil.h
@@ -1,10 +1,26 @@
 #pragma once
 #include <cmath>
+#include "Library/Location2D.h"
+#include "MyDxLib.h"
 
 namespace MathUtil {
 
     const float PI_F = 3.14159265f;
     const double PI_D = 3.14159265358979323846;
+
+    inline float ToRadian(float degree) {
+        return(degree * (PI_F / 180.0f));
+    }
+    inline float ToDegree(float radian) {
+        return(radian * (180.0f / PI_F));
+    }
+
+    inline Location2D WorldToScreen(const Location2D& worldPos) {
+        Location2D result;
+        result.x_ = worldPos.x_;
+        result.y_ = Screen::HEIGHT - worldPos.y_;
+        return result;
+    }
 
     namespace Lerp {
         inline float easeInOutSine(float x) {
