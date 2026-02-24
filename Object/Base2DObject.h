3693bbce463857f78d4c65820a6528f329c2fbb3
250190 <250190@jc-21.jp>
2026-02-24 09:39:32 +0900

    ＜追加＞　Base2DObjectに方向・半径・回転速度・回転角を追加しました

--- Object/Base2DObject.h
+++ Object/Base2DObject.h
@@ -6,8 +6,12 @@
 
 class Base2DObject : public BaseObject {
 protected:
-    Location2D location_;
-    Vector2D vector_;
+    Location2D location_; //ˆÊ’u
+    Vector2D vector_; //‘¬‚³
+    Vector2D direction_; //•ûŒü
+    float radius_; //”¼Œa
+    float omega_; //‰ñ“]‘¬“x
+    float angle_; //‰ñ“]Šp
 public:
     Base2DObject();
     Base2DObject(const std::string& name, const Location2D& location, const Vector2D& vector, const bool& isAlive);
@@ -17,5 +21,13 @@ public:
     void SetLocation(const Location2D& loc) { location_ = loc; };
     Vector2D GetVector() { return vector_;  };
     void SetVector(const Vector2D& vec) { vector_ = vec; };
+    Vector2D GetDirection() { return direction_; }
+    void SetDirection(const Vector2D& dir) { direction_ = dir; }
+    void SetRadius(float r) { radius_ = r; }
+    float GetRadius() { return radius_; }
+    void SetOmega(float omega) { omega_ = omega; };
+    float GetOmega() { return omega_; }
+    void SetAngle(float angle) { angle_ = angle; }
+    float GetAngle() { return angle_; }
 };
 
