cededdb12fc01ef17d970bc0de2d0418917a4ff5
TokumeiKibou01 <snowsansub011307@gmail.com>
2026-02-27 00:29:37 +0900

    ＜追加＞　敵をスポーンさせる関数を追加しました

--- Library/ObjectFactory.h
+++ Library/ObjectFactory.h
@@ -2,6 +2,7 @@
 #include "../Manager/ObjectManager.h"
 #include "../Library/Location2D.h"
 #include "../Library/Vector2D.h"
+#include "../MathUtil.h"
 #include "../Object/Bullet.h"
 #include "../Scene/BaseScene.h"
 
@@ -12,7 +13,24 @@ namespace ObjectFactory {
     }
 
     inline void spawnEnemy() {
+        Location2D location;
 
+        int side = GetRand(3);
+        if (side == 0) { location.x_ = 0.0f; location.y_ = MathUtil::Rand::RandRange(0.0f, Screen::HEIGHT); }
+        if (side == 1) { location.x_ = Screen::WIDTH; location.y_ = MathUtil::Rand::RandRange(0.0f, Screen::HEIGHT); } 
+        else if (side == 2) { location.x_ = MathUtil::Rand::RandRange(0.0f, Screen::WIDTH); location.y_ = 0.0f; } 
+        else if (side == 3) { location.x_ = MathUtil::Rand::RandRange(0.0f, Screen::WIDTH); location.y_ = Screen::HEIGHT; }
+
+        float angle = MathUtil::Rand::RandRange(0.0f, MathUtil::PI_F * 2.0f);
+        float speed = MathUtil::Rand::RandRange(EnemyParams::SPEED_MIN, EnemyParams::SPEED_MAX);
+        Vector2D velocity;
+        velocity.x_ = cosf(angle) * speed;
+        velocity.y_ = sinf(angle) * speed;
+        int segment = MathUtil::Rand::RandRangeInt(EnemyParams::SEGMENT_MIN, EnemyParams::SEGMENT_MAX);
+
+        ObjectManager& objManager = ObjectManager::GetInstance();
+        Enemy* enemy = new Enemy(EnemyType::LARGE, segment);
+        objManager.AddObject("RunningScene", enemy);
     }
 
     inline void spawnBullet(BaseScene* scene, const Location2D& loc, const Vector2D& dir, const float offset) {
